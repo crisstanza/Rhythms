f7afa5befd93a05b5e16e27972aa5f2907ff55e7
Cristiano Neves <cristiano.neves@datacom.ind.br>
2014-09-22 17:40:45 -0300

    New logic do create the rithm.

--- js/js.js
+++ js/js.js
@@ -1,28 +1,31 @@
-	(function() {
+(function() {
 
 	function draw() {
-		var myPre = document.getElementById('my-pre');
-		var rDec = Math.floor((Math.random() * 15) + 1);
-		var rBin = rDec.toString(2);
-		var sb = [];
-		sb.push('g -' + spaceIt(padLeft(rBin, 4, 0)) + '-\n');
-		sb.push('d -' + spaceIt(padLeft('', 4, ' ')) + '-\n');
-		sb.push('a -' + spaceIt(padLeft('', 4, ' ')) + '-\n');
-		sb.push('e -' + spaceIt(padLeft('', 4, ' ')) + '-\n');
-		sb.reverse();
-		myPre.innerHTML = sb.join('');
+		var allStrings = document.getElementById('all-strings');
+		var strings = allStrings.querySelectorAll('div[id^=string-]');
+		var note = nextNote();
+		drawNotes(strings, note);
+		drawEnd(strings);
 	}
 
-	function padLeft(str, n, pad){
-		return Array(n - String(str).length + 1).join(pad||'0') + str;
+	function nextNote() {
+		return ['a', 'b', 'c', 'd'];
 	}
 
-	function spaceIt(str) {
-		return str.split('').join('-');
+	function drawNotes(strings, note) {
+		var length = strings.length;
+		for (var i = 0 ; i < length ; i++ ) {
+			var string = strings[i];
+			string.innerHTML += (note[i] + '-');
+		}
 	}
 
-	function noZeroes(str) {
-		return str.replace(/0/g, ' ');
+	function drawEnd(strings) {
+		var length = strings.length;
+		for (var i = 0 ; i < length ; i++ ) {
+			var string = strings[i];
+			string.innerHTML += '|';
+		}
 	}
 
 	function init() {
@@ -31,4 +34,4 @@
 
 	window.addEventListener('load', init, false);
 
-	})();
+})();
